a1db7a4a5a298eeaaa04a3e0dec15d3f30c8e335
wdonayredilate <78710915+wdonayredilate@users.noreply.github.com>
2024-01-12 06:27:15 +0000

    bump check

--- wp-openstreetmap.php
+++ wp-openstreetmap.php
@@ -3,7 +3,7 @@
    * Plugin Name:     Dilate WP OpenstreetMap
    * Plugin URI:      https://www.dilate.com.au
    * Description:     Use openstreetmap on your site
-   * Version:         0.0.0
+   * Version:         0.0.1
    * Author:          William Donayre Jr. - Dilate Digital LLC
    * Author URI:      https://www.dilate.com.au
    * Text Domain:     wp-openstreetmap
